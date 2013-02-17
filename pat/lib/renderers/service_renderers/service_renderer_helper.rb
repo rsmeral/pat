dd031aae27db69491313b83fb9329fb6b55fd991
Oliver Kiss <okiss@redhat.com>
2013-02-17 15:55:15 +0100

    Fixed message_from_event

--- pat/lib/renderers/service_renderers/service_renderer_helper.rb
+++ pat/lib/renderers/service_renderers/service_renderer_helper.rb
@@ -1,15 +1,11 @@
 module ServiceRendererHelper
 
   def message_from_event(event)
-    message = Message.new
+    message = process_event(event)
     message.day = event.time.strftime("%b %d")
     message.time = event.time.strftime("%H:%M")
     message.person = event.person.name
     message.service = event.service.id
-
-    event_data = process_event(event)
-    message.header = event_data.header
-    message.content = event_data.content
 
     message
   end
